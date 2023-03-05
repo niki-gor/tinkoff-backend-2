006698f5458f6d60e353a833766de99b1c613ce8
niki-gor <nic.gor@yandex.ru>
2023-03-05 14:36:09 +0300

    Remove useless comments

--- forum/resources/strings.py
+++ forum/resources/strings.py
@@ -1,8 +1,2 @@
-# from forum.api.models import Error
-
-# ErrUserNotFound = Error(detail='user not found')
-# ErrAlreadyFriends = Error(detail='already friends')
-
-
 USER_NOT_FOUND = 'user not found'
 ALREADY_FRIENDS = 'already friends'
