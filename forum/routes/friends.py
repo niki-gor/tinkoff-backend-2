f5fb471b0b7df79049ecfb5b685ae172c5195443
niki-gor <nic.gor@yandex.ru>
2023-04-23 12:05:13 +0300

    Process values out of int32 range correctly

--- forum/routes/friends.py
+++ forum/routes/friends.py
@@ -2,21 +2,23 @@ from fastapi import APIRouter, Depends, Response, status
 from fastapi.exceptions import HTTPException
 
 from forum.dependencies.authentication import authenticate_user_id
-from forum.dependencies.database import get_friends_repo
+from forum.dependencies.database import get_friends_repo, get_users_repo
 from forum.models.schemas import ListOfUsersInResponse
-from forum.repositories.base import BaseFriendsRepository
+from forum.repositories.base import BaseFriendsRepository, BaseUsersRepository
 from forum.resources import strings
+from forum.routes.users import validate_user_id
 
 router = APIRouter()
 
 
 @router.put("/{to_id}", status_code=status.HTTP_201_CREATED, response_class=Response)
 async def befriend(
-    user_id: int,
     to_id: int,
+    user_id: int = Depends(validate_user_id),
     auth_user_id: int = Depends(authenticate_user_id),
     friendships: BaseFriendsRepository = Depends(get_friends_repo),
 ) -> None:
+    await validate_user_id(to_id)
     if user_id != auth_user_id:
         raise HTTPException(
             status_code=status.HTTP_403_FORBIDDEN,
@@ -38,7 +40,14 @@ async def befriend(
 
 @router.get("", response_model=ListOfUsersInResponse)
 async def get_friends(
-    user_id: int, friendships: BaseFriendsRepository = Depends(get_friends_repo)
+    user_id: int = Depends(validate_user_id),
+    users: BaseUsersRepository = Depends(get_users_repo),
+    friendships: BaseFriendsRepository = Depends(get_friends_repo),
 ) -> ListOfUsersInResponse:
+    exists = await users.get_user_by_id(user_id)
+    if not exists:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail=strings.USER_NOT_FOUND
+        )
     friends = await friendships.get_friends(user_id)
     return ListOfUsersInResponse(users=friends)
--- forum/routes/users.py
+++ forum/routes/users.py
@@ -17,6 +17,14 @@ from forum.resources import strings
 router = APIRouter()
 
 
+async def validate_user_id(user_id: int) -> int:
+    if not (-2147483648 <= user_id and user_id <= 2147483647):  # int32
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail=strings.USER_NOT_FOUND
+        )
+    return user_id
+
+
 @router.post("", status_code=status.HTTP_201_CREATED, response_model=UserIdInResponse)
 async def create_user(
     user_info: UserInCreate,
@@ -36,7 +44,8 @@ async def get_all_users(
 
 @router.get("/{user_id}", response_model=UserInResponse)
 async def get_user(
-    user_id: int, users: BaseUsersRepository = Depends(get_users_repo)
+    user_id: int = Depends(validate_user_id),
+    users: BaseUsersRepository = Depends(get_users_repo),
 ) -> UserInResponse:
     user_with_passwd = await users.get_user_by_id(user_id)
     if user_with_passwd is None:
@@ -51,8 +60,8 @@ async def get_user(
     "/{user_id}", status_code=status.HTTP_204_NO_CONTENT, response_class=Response
 )
 async def edit_user(
-    user_id: int,
     user_info: UserInUpdate,
+    user_id: int = Depends(validate_user_id),
     auth_user_id: int = Depends(authenticate_user_id),
     users: BaseUsersRepository = Depends(get_users_repo),
 ) -> None:
