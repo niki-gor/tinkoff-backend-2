47dadcc7996a395125baf4ed06b088b77399be4a
niki-gor <nic.gor@yandex.ru>
2023-05-04 01:09:46 +0300

    Use prometheus, node-exporter and grafana

--- forum/__main__.py
+++ forum/__main__.py
@@ -1,7 +1,11 @@
 import uvicorn
 
 from forum.core.app import get_application
+from prometheus_fastapi_instrumentator import Instrumentator
+
 
 if __name__ == "__main__":
     app = get_application()
+    Instrumentator().instrument(app).expose(app)
+
     uvicorn.run(app=app, port=8081, host="0.0.0.0")
